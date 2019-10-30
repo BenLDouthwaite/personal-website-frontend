9ed4694d8181db7ed93f87c41451fb19ec79c220
Ben L Douthwaite <Ben.L.Douthwaite@gmail.com>
2019-10-30 22:49:38 +0900

    Text update to test pipeline

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello World. This is a dummy update</h1>;
+    return <h1>Hello World. This is a dummy update. And soon it will be a whole application! Once I have time. I'm super busy</h1>;
   }
 }
 
