dfa53423c24ec153a3a729994589c3a0aaffb2c2
Ben L Douthwaite <Ben.L.Douthwaite@gmail.com>
2019-11-03 21:56:29 +0900

    Adding dummy call to rest emdpoint for testing docker network comminications

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello World. This is a dummy update. And soon it will be a whole application! Once I have time. I'm super busy</h1>;
+    return <h1>Hello World. This is a dummy update. And soon it will be a whole application! Once I have time. I'm super busy. Also. I want this to look pretty</h1>;
   }
+
+  componentWillMount() {
+  	console.log("about to mount yo")
+  	fetch("http://localhost:8081/hello")
+  		.then(response => response.json())
+  		.then(res => {
+  			console.log(res)
+  		})
+  }
+
 }
 
 export default App;
